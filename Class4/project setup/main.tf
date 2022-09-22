945b5341f01e4537313a0b145bc6d31e6f0c1655
kairasultann <kairasultann@gmail.com>
2022-09-22 08:41:24 +0000

    added nuli_resource for config set

--- Class4/project setup/main.tf
+++ Class4/project setup/main.tf
@@ -1,34 +1,44 @@
 data "google_billing_account" "acct" {
-	display_name = "My Billing Account"
-	open = true
+  display_name = "My Billing Account"
+  open         = true
 }
-output id {
-    value = data.google_billing_account.acct.id
-}
-
-output name {
-    value = data.google_billing_account.acct.name
+output "id" {
+  value = data.google_billing_account.acct.id
 }
 
-output display_name {
-    value = data.google_billing_account.acct.display_name
+output "name" {
+  value = data.google_billing_account.acct.name
+}
+
+output "display_name" {
+  value = data.google_billing_account.acct.display_name
 }
 
 resource "random_password" "password" {
-	length = 16
-	number = false
-	special = false
-	lower = true
-	upper = false
+  length  = 16
+  number  = false
+  special = false
+  lower   = true
+  upper   = false
 }
 
 
 resource "google_project" "gcp-project-team3" {
-	name = "gcp-project-team3"
-	project_id = random_password.password.result
-	billing_account = data.google_billing_account.acct.id
+  name            = "gcp-project-team3"
+  project_id      = random_password.password.result
+  billing_account = data.google_billing_account.acct.id
 }
 
-output project_id {
-    value = google_project.gcp-project-team3.id
+output "project_id" {
+  value = google_project.gcp-project-team3.id
+}
+
+resource "null_resource" "set-project" {
+  triggers = {
+    always_run = "${timestamp()}"
+  }
+
+  provisioner "local-exec" {
+    command = "gcloud config set project ${google_project.gcp-project-team3.id}"
+  }
 }
